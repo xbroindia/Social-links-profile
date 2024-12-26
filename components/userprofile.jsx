d899ef3dafcb8cf447dc0ef52252faffe9226275
xbroindia <155175426+xbroindia@users.noreply.github.com>
2024-12-26 16:27:02 +0530

    upodated

--- components/userprofile.jsx
+++ components/userprofile.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './userprofile.css'
+import pic1 from './avatar-jessica.jpeg'
 
 export default function Userprofile(){
 
     return(
     <div className='userprofile'>
-        <img src="./assets\images\avatar-jessica.jpeg" alt="user" className='userpic'/>
+        <img src={pic1} alt="user" className='userpic'/>
        <h2>Jessica Randall</h2> 
     <p className='para1'>London, United Kingdom</p>
    <p className='para2'>"Front-end developer and avid reader."</p>
