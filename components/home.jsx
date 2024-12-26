efb6f57c0e0f4c44f2354232acc65bd9cf2a5fa5
xbroindia <155175426+xbroindia@users.noreply.github.com>
2024-12-26 16:41:04 +0530

    Updated

--- components/home.jsx
+++ components/home.jsx
@@ -11,6 +11,7 @@ export default function Home() {
     <Userprofile/>
     <Social_medias/>
     </div>
+    <Link/>
     </>
   )
 }
--- components/link.jsx
+++ components/link.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import './link.css'
 
 export default function Link() {
   return (
-    <div>
-    <div class="attribution">
+    <div className='link'>
     Challenge by 
     <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by 
-    <a href="#">Shah Hamdan</a>
-  </div></div>
+    <a href="https://www.instagram.com/shahhamdan165/">Shah Hamdan</a>
+  </div>
   )
 }
